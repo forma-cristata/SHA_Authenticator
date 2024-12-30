416567e138bf0b11a2a542cf9f0997d2d8ff5c50
kcraycraft45 <kaaucri@gmail.com>
2024-12-30 15:39:56 -0500

    VueJSBaseStructure

---
+++ src/client/src/components/GithubRedirect.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: "GithubRedirect.vue"
+}
+</script>
+
+<template>
+<div id="oath-link" class="col-6">
+
+</div>
+</template>
+
+<style scoped>
+#oath-link{
+background-color: #2b3137;
+}
+</style>
