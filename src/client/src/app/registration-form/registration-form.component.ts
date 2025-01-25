ed9d1c068ab7ce8ab4c3418e179bd1f3b54e74d3
kcraycraft45 <kcraycraft45@southhills.edu>
2025-01-25 14:32:27 -0500

    Cookie Saved for Username

--- src/client/src/app/registration-form/registration-form.component.ts
+++ src/client/src/app/registration-form/registration-form.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-registration-form',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './registration-form.component.html',
   standalone: true,
   styleUrl: './registration-form.component.css'
 })
 export class RegistrationFormComponent {
+  public termLengthInSeconds: string = new Date(Date.now() + 24 * 60 * 60 * 7 * 14 * 1000).toUTCString();
+  public username: string = '';
+  ngOnInit() {
+      const termLengthInSeconds = new Date(Date.now() + 24 * 60 * 60 * 7 * 14 * 1000).toUTCString();
+  }
 
+  createUsernameCookie(event: Event) {
+    // @ts-ignore
+    let username = this.username;
+    console.log(username);
+    document.cookie = `username=${username}; expires=${(this.termLengthInSeconds)}`;
+    this.username = '';
+    // Preventing default behavior by returning false
+    return false;
+  }
 }
